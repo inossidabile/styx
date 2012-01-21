d8d35203e7d60b3ed368710c93ae0da0a8eca059
Boris Staal <boris@roundlake.ru>
2012-01-22 00:14:48 +0700

    Specs corrected

--- spec/controllers/tests_controller_spec.rb
+++ spec/controllers/tests_controller_spec.rb
@@ -6,6 +6,6 @@ describe TestsController do
   it 'index' do
     get :index
     response.body.should have_content('Styx.Initializers.Tests.initialize({"data":"test"})')
-    response.body.should have_content('Styx.Initializers.Tests.index({"data":"test"})')
+    response.body.should have_content('Styx.Initializers.Tests[\'index\']({"data":"test"})')
   end
 end
